124106de31396b92a5db6c5ffd2537c07d821d2b
Chance Smith <chancesmith97@hotmail.com>
2019-07-04 00:51:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,6 +19,8 @@ class Pokemon
     pokemon = database_connection.execute("SELECT * FROM pokemon WHERE id = ?", id).flatten
     name = pokemon[1]
     type = pokemon[2]
+    
+    pokemon_inst = Pokemon.new(id: id, name: name, type: type, hp: hp, db: database_connection)
   end
     
   
