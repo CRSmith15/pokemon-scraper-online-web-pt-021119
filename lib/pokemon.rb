76158cc6d95dc644c240b267a33bc4800db3c03a
Chance Smith <chancesmith97@hotmail.com>
2019-07-04 00:39:59 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,9 @@
 class Pokemon
   
-  attr_accessor :name, :type
+  attr_accessor :name, :type, :db, :hp
   attr_reader :id
   
-  def initialize(name, type, id=nil)
+  def initialize(name, type, id=nil, db)
     @id = id 
     @name = name 
     @type = type
